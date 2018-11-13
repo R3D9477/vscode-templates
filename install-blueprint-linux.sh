f6a59313b1b61aea20d909265f4f28c01fe8b76c
r3d9u11 <r3d9u11@gmail.com>
2018-11-13 14:19:32 +0300

    msg

--- install-blueprint-linux.sh
+++ install-blueprint-linux.sh
@@ -3,10 +3,6 @@
 TARGET_DIR=vscode-templates-blueprint
 TEMPLATE_FMT=__pascalCase_name__
 
-echo "IN PROGRESS..."
-echo "Wait, please."
-echo ""
-
 git clone --recurse-submodules --quiet git://github.com/r3d9u11/vscode-templates.git $TARGET_DIR 2> /dev/null
 
 shopt -s extglob
@@ -29,7 +25,7 @@ find "$TARGET_DIR" -name "README.md" -print0 | xargs -0 rm -rf
 echo ""
 echo ""
 echo "DONE."
-echo "Please, add the next lines into section \"blueprint.templatesPath\" of your settings and restart VSCode:"
+echo "NOTE: Please, add the next line into section \"blueprint.templatesPath\" of your settings and restart VSCode:"
 echo ""
 echo "\"$PWD/$TARGET_DIR\""
 echo ""
