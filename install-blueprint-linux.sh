f661f1b9d60de6871b8fc15215261aa19413ae7e
r3d9u11 <r3d9u11@gmail.com>
2018-11-13 13:12:29 +0300

    bp improve

--- install-blueprint-linux.sh
+++ install-blueprint-linux.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 TARGET_DIR=vscode-templates-blueprint
-TEMPLATE_NAME=__pascalCase_name__
+TEMPLATE_FMT=__pascalCase_name__
 
 git clone --recurse-submodules git://github.com/r3d9u11/vscode-templates.git $TARGET_DIR
 
 shopt -s extglob
 
+SET=""
+
 for LANG_DIR in $TARGET_DIR/*; do
     for TEMPLATE_DIR in $LANG_DIR/*; do
-        mv $TEMPLATE_DIR "$LANG_DIR - $(basename ${TEMPLATE_DIR//_/ })"
+        if [ -d "$TEMPLATE_DIR" ]; then
+            TEMPLATE_PATH=$(basename "$TEMPLATE_DIR")
+            TEMPLATE_PATH="$LANG_DIR - ${TEMPLATE_PATH//_/ }"
+            mkdir -p "$TEMPLATE_PATH"
+            mv "$TEMPLATE_DIR" "$TEMPLATE_PATH/$TEMPLATE_FMT"
+            SET+="\"$TEMPLATE_PATH\",\n"
+        fi
     done
+    rm -rf "$LANG_DIR"
 done
+
+find "$TARGET_DIR" -name ".git*" -print0 | xargs -0 rm -rf
+find "$TARGET_DIR" -name "README.md" -print0 | xargs -0 rm -rf
 
 echo ""
 echo ""
 echo "DONE."
 echo "Please, add the following lines into section \"blueprint.templatesPath\" of your settings and restart VSCode:"
 echo ""
-echo "\"$PWD/$TARGET_DIR/c\","
-echo "\"$PWD/$TARGET_DIR/cpp\","
-echo "\"$PWD/$TARGET_DIR/csharp\","
-echo "\"$PWD/$TARGET_DIR/haxe\","
-echo "\"$PWD/$TARGET_DIR/javascript\","
-echo "\"$PWD/$TARGET_DIR/nim\""
-echo ""
-echo ""
+echo -e $SET
