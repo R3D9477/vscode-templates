4c02844ed6034fb4ac7a0e295976ba172cbe1344
r3d9u11 <r3d9u11@gmail.com>
2018-11-13 14:00:52 +0300

    post-install hint

--- install-blueprint-linux.sh
+++ install-blueprint-linux.sh
@@ -7,8 +7,6 @@ git clone --recurse-submodules git://github.com/r3d9u11/vscode-templates.git $TA
 
 shopt -s extglob
 
-SET=""
-
 for LANG_DIR in $TARGET_DIR/*; do
     for TEMPLATE_DIR in $LANG_DIR/*; do
         if [ -d "$TEMPLATE_DIR" ]; then
@@ -16,7 +14,6 @@ for LANG_DIR in $TARGET_DIR/*; do
             TEMPLATE_PATH="$LANG_DIR - ${TEMPLATE_PATH//_/ }"
             mkdir -p "$TEMPLATE_PATH"
             mv "$TEMPLATE_DIR" "$TEMPLATE_PATH/$TEMPLATE_FMT"
-            SET+="\"$PWD/$TEMPLATE_PATH\",\n"
         fi
     done
     rm -rf "$LANG_DIR"
@@ -28,6 +25,6 @@ find "$TARGET_DIR" -name "README.md" -print0 | xargs -0 rm -rf
 echo ""
 echo ""
 echo "DONE."
-echo "Please, add the following lines into section \"blueprint.templatesPath\" of your settings and restart VSCode:"
+echo "Please, add the next lines into section \"blueprint.templatesPath\" of your settings and restart VSCode:"
 echo ""
-echo -e $SET
+echo "\"$PWD/$TARGET_DIR\"\n"
